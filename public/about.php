701bda54cc1c4e7f268f92ca476de31e31df0f1c
nazli <n.eroglux014@gmail.com>
2025-09-19 13:02:40 +0200

    yum

--- public/about.php
+++ public/about.php
@@ -1,17 +1,11 @@
-<?php
-phpinfo();
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - Music Library</title>
+    <title>Document</title>
 </head>
 <body>
-
-
+    
 </body>
 </html>
-?>
