aabd2fe2e7527ae32a8205c39a61666e09329313
nazli <n.eroglux014@gmail.com>
2025-09-19 14:32:13 +0200

    ouioui

--- public/single.php
+++ public/single.php
@@ -1,16 +1,84 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-      <div class="container">
-      <h1 class="mt-5">Welkom bij de Music Library</h1>
-      <p>Dit is de homepagina van je project.</p>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                       data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
     </div>
-    
+</nav>
+
+<main class="container my-5">
+    <h1 class="mt-5">Dit is mijn eerste titel</h1>
+    <div class="col-lg-8 px-0">
+        <p class="fs-5">
+            Aute exercitation pariatur eiusmod. Pariatur eiusmod magna sit in velit, quis esse. Sit in velit quis esse exercitation. Velit quis esse, exercitation reprehenderit culpa qui officia.
+        </p>
+        <p>
+            Id voluptate in ipsum magna est anim, eiusmod. Ipsum magna est anim, eiusmod non. Anim eiusmod, non consectetur. Consectetur minim et incididunt tempor nisi proident labore. Et incididunt tempor nisi proident, labore eu ea. Nisi proident labore eu ea, laborum dolore. Eu ea laborum dolore ullamco.
+
+            Voluptate in ipsum magna est, anim eiusmod. Magna est anim eiusmod non consectetur. Anim eiusmod, non consectetur. Consectetur minim et incididunt tempor nisi proident labore. Et incididunt tempor nisi proident, labore eu ea.
+
+            In ipsum magna est. Magna est anim eiusmod non consectetur. Anim eiusmod, non consectetur. Consectetur minim et incididunt tempor nisi proident labore. Et incididunt tempor nisi proident, labore eu ea. Nisi proident labore eu ea, laborum dolore. Eu ea laborum dolore ullamco. Laborum dolore ullamco consequat eu.
+
+            Ipsum magna est anim, eiusmod non. Anim eiusmod, non consectetur. Consectetur minim et incididunt tempor nisi proident labore. Et incididunt tempor nisi proident, labore eu ea. Nisi proident labore eu ea, laborum dolore. Eu ea laborum dolore ullamco. Laborum dolore ullamco consequat eu. Ullamco consequat eu sed quis mollit laborum. Eu sed quis mollit laborum. Quis mollit laborum, duis voluptate aliquip.
+
+            Magna est anim eiusmod non consectetur. Anim eiusmod, non consectetur. Consectetur minim et incididunt tempor nisi proident labore. Et incididunt tempor nisi proident, labore eu ea. Nisi proident labore eu ea, laborum dolore. Eu ea laborum dolore ullamco. Laborum dolore ullamco consequat eu. Ullamco consequat eu sed quis mollit laborum. Eu sed quis mollit laborum.
+        </p>
+    </div>
+</main>
+
+<footer class="footer mt-auto py-3 bg-body-tertiary">
+    <div class="container">
+        <span class="text-body-secondary">Place footer content here.</span>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </body>
 </html>
